bb5744410108f248d8825bb6241fe7544b89fe11
ITurres <emanueliturres.co@gmail.com>
2022-11-08 13:17:00 -0300

    Add navbar onscroll-animation

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -1,5 +1,6 @@
-//?BELOW NAVBAR HOVER / ANIMATIONS//
 //! why animate a hover? in order to change the logo file on hover//
+import { $sidePanel_closebtn } from "./sidepanel.js";
+
 const $navbar = document.querySelector("[data-navbar]");
 const $navbarItems = document.querySelectorAll("[data-navbar-item]");
 const $headerContent = document.querySelector("[data-header-content]");
@@ -10,7 +11,9 @@ const bgColor2 = "var(--style-color-white)";
 const fontColor1 = "var(--style-color-white)";
 const fontColor2 = "var(--style-color-green)";
 
-const navbarHoverAnimation = (src, bgColor, textColor) => {
+//?BELOW---NAVBAR HOVER ANIMATIONS//
+
+export const navbarHoverAnimation = (src, bgColor, textColor) => {
   $navbar.style.background = bgColor;
   $navbarItems.forEach((element) => {
     element.style.color = textColor;
@@ -19,23 +22,51 @@ const navbarHoverAnimation = (src, bgColor, textColor) => {
   $navbar.style.transition = "0.5s";
 };
 
-$navbar.addEventListener("mouseover", () => {
-  navbarHoverAnimation(colorLogoSrc, bgColor2, fontColor2);
+// const hoverWhenMouse = (possible) => {
+//   if (possible) {
+    $navbar.addEventListener("mouseover", () => {
+      navbarHoverAnimation(colorLogoSrc, bgColor2, fontColor2);
+    });
+    $navbar.addEventListener("mouseout", () => {
+      navbarHoverAnimation(whiteLogoSrc, bgColor1, fontColor1);
+    });
+//   }
+// };
+
+//?ABOVE---NAVBAR HOVER ANIMATIONS//
+
+//?BELOW NAVBAR ON-SCROLL ANIMATION//
+
+let lastScrollTop = 0;
+window.addEventListener("scroll", () => {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    $navbar.style.top = "-100px";
+  } else {
+    $navbar.style.top = "0";
+    navbarHoverAnimation(colorLogoSrc, bgColor2, fontColor2);
+  }
+  lastScrollTop = scrollTop;
+  if (scrollTop === 0) {
+    //*so when scroll reaches top of page, logo turns back to white and else (see variables)/
+    navbarHoverAnimation(whiteLogoSrc, bgColor1, fontColor1);
+    // hoverWhenMouse(true);
+  }
 });
 
-$navbar.addEventListener("mouseout", () => {
-  navbarHoverAnimation(whiteLogoSrc, bgColor1, fontColor1);
-});
+//?ABOVE NAVBAR ON-SCROLL ANIMATION//
 
-//?ABOVE NAVBAR HOVER ANIMATIONS//
-
-//?BELOW NAVBAR OPEN-CLOSE ANIMATION//
+//?BELOW---NAVBAR OPEN-CLOSE ANIMATION//
 
 const $openBtn = document.querySelector("[data-open-menu-btn]");
 
 $openBtn.addEventListener("click", () => {
   hideNavbar();
   darkenSection();
+});
+
+$sidePanel_closebtn.addEventListener("click", () => {
+  navbarHoverAnimation(colorLogoSrc, bgColor2, fontColor2);
 });
 
 export const darkenSection = () => {
@@ -52,4 +83,4 @@ export const showNavbar = () => {
   $navbar.style.top = "0";
 };
 
-//?ABOVE NAVBAR OPEN-CLOSE ANIMATION//
+//?ABOVE---AVBAR OPEN-CLOSE ANIMATION//
