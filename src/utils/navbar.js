0bd7f6223960f340cb2c1b594e2bedaa1dc9b9bd
ITurres <emanueliturres.co@gmail.com>
2022-11-08 09:55:26 -0300

    Refactor avbar-hover animation: Due to possible future animation

--- src/utils/navbar.js
+++ src/utils/navbar.js
@@ -3,23 +3,30 @@
 const $navbar = document.querySelector("[data-navbar]");
 const $navbarItems = document.querySelectorAll("[data-navbar-item]");
 const $headerContent = document.querySelector("[data-header-content]");
+const whiteLogoSrc = "/src/assets/media/img/logo/ttm-logo-white.png";
+const colorLogoSrc = "/src/assets/media/img/logo/ttm-logo-color.png";
+const bgColor1 = "transparent";
+const bgColor2 = "var(--style-color-white)";
+const fontColor1 = "var(--style-color-white)";
+const fontColor2 = "var(--style-color-green)";
 
-const navbarHoverAnimation = (src) => {
-  $navbar.classList.toggle("navbar--white"); //!do we need this?//
+const navbarHoverAnimation = (src, bgColor, textColor) => {
+  $navbar.style.background = bgColor;
   $navbarItems.forEach((element) => {
-    element.classList.toggle("navbar__item--green");
+    element.style.color = textColor;
   });
   $navbar.children[1].children[0].src = src;
   $navbar.style.transition = "0.5s";
 };
 
 $navbar.addEventListener("mouseover", () => {
-  navbarHoverAnimation("/src/assets/media/img/logo/ttm-logo-color.png");
+  navbarHoverAnimation(colorLogoSrc, bgColor2, fontColor2);
 });
 
 $navbar.addEventListener("mouseout", () => {
-  navbarHoverAnimation("/src/assets/media/img/logo/ttm-logo-white.png");
+  navbarHoverAnimation(whiteLogoSrc, bgColor1, fontColor1);
 });
+
 //?ABOVE NAVBAR HOVER ANIMATIONS//
 
 //?BELOW NAVBAR OPEN-CLOSE ANIMATION//
