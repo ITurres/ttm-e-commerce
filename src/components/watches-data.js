750d7ab852f6d4f01f9d65ea126baa8f58b579d0
ITurres <emanueliturres.co@gmail.com>
2022-11-09 11:27:19 -0300

    Replace array of data with object / Update key names

--- src/components/watches-data.js
+++ src/components/watches-data.js
@@ -1,74 +1,74 @@
-export const watches_data = [
-  {
+export const watches_data = {
+  seadweller: {
     href: "#",
-    srcset:
+    roller_srcset:
       "https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0005_m126603-0001-sea-dweller.jpg?imwidth=550,%20https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0005_m126603-0001-sea-dweller.jpg?imwidth=1080%202x",
-    src: "/src/assets/media/img/img-watches-roller/watches-sea-dweller.webp",
-    alt: "Rolex deepSea watch",
-    title: "sea-dweller",
-    about: "The watch that has conquered the depths",
+    roller_src: "/src/assets/media/img/img-watches-roller/watches-sea-dweller.webp",
+    img_alt: "Rolex deepSea watch",
+    roller_title: "sea-dweller",
+    roller_about: "The watch that has conquered the depths",
   },
-  {
+  airking: {
     href: "#",
-    srcset:
+    roller_srcset:
       "https://content.rolex.com/dam/new-watches-2022/homepage/roller-family/homepage-new-watches-2022-roller-watches-air-king-family-m126900-0001.jpg?imwidth=550,%20https://content.rolex.com/dam/new-watches-2022/homepage/roller-family/homepage-new-watches-2022-roller-watches-air-king-family-m126900-0001.jpg?imwidth=1080%202x",
-    src: "/src/assets/media/img/img-watches-roller/watches-air-king.webp",
-    alt: "Rolex air-king watch",
-    title: "air-king",
-    about: "A tribute to aviation",
+    roller_src: "/src/assets/media/img/img-watches-roller/watches-air-king.webp",
+    img_alt: "Rolex air-king watch",
+    roller_title: "air-king",
+    roller_about: "A tribute to aviation",
   },
-  {
+  gmtmaster: {
     href: "#",
-    srcset:
+    roller_srcset:
       "https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0004_m126711chnr-0002-gmt-master-ii.jpg?imwidth=550,%20https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0004_m126711chnr-0002-gmt-master-ii.jpg?imwidth=1080%202x",
-    src: "/src/assets/media/img/img-watches-roller/watches-gmt-master-ii.webp",
-    alt: "Rolex gmt-master 2 watch",
-    title: "gmt - master ii",
-    about: "The cosmopolitan watch",
+    roller_src: "/src/assets/media/img/img-watches-roller/watches-gmt-master-ii.webp",
+    img_alt: "Rolex gmt-master 2 watch",
+    roller_title: "gmt - master ii",
+    roller_about: "The cosmopolitan watch",
   },
-  {
+  daydate: {
     href: "#",
-    srcset:
+    roller_srcset:
       "https://content.rolex.com/dam/new-watches-2022/homepage/roller-family/homepage-new-watches-2022-roller-watches-day-date-family-m228236-0012.jpg?imwidth=550,%20https://content.rolex.com/dam/new-watches-2022/homepage/roller-family/homepage-new-watches-2022-roller-watches-day-date-family-m228236-0012.jpg?imwidth=1080%202x",
-    src: "/src/assets/media/img/img-watches-roller/watches-day-date.webp",
-    alt: "Rolex day - date watch",
-    title: "day - date",
-    about: "The materialization of an ideal",
+    roller_src: "/src/assets/media/img/img-watches-roller/watches-day-date.webp",
+    img_alt: "Rolex day - date watch",
+    roller_title: "day - date",
+    roller_about: "The materialization of an ideal",
   },
-  {
+  daytona: {
     href: "#",
-    srcset:
+    roller_srcset:
       "https://content.rolex.com/dam/new-watches-2021/homepage/roller/all-watches/watches_0012_m116519ln-0038-cosmograph-daytona.jpg?imwidth=550,%20https://content.rolex.com/dam/new-watches-2021/homepage/roller/all-watches/watches_0012_m116519ln-0038-cosmograph-daytona.jpg?imwidth=1080%202x",
-    src: "/src/assets/media/img/img-watches-roller/watches-cosmograph-daytona.webp",
-    alt: "Rolex daytona watch",
-    title: "cosmograph daytona",
-    about: "Born for competition",
+    roller_src: "/src/assets/media/img/img-watches-roller/watches-cosmograph-daytona.webp",
+    img_alt: "Rolex daytona watch",
+    roller_title: "cosmograph daytona",
+    roller_about: "Born for competition",
   },
-  {
+  skydweller: {
     href: "#",
-    srcset:
+    roller_srcset:
       "https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0000_m326238-0009-sky-dweller.jpg?imwidth=550,%20https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0000_m326238-0009-sky-dweller.jpg?imwidth=1080%202x",
-    src: "/src/assets/media/img/img-watches-roller/watches-sky-dweller.webp",
-    alt: "Rolex sky-dweller watch",
-    title: "sky - dweller",
-    about: "The time anytime, anywhere",
+    roller_src: "/src/assets/media/img/img-watches-roller/watches-sky-dweller.webp",
+    img_alt: "Rolex sky-dweller watch",
+    roller_title: "sky - dweller",
+    roller_about: "The time anytime, anywhere",
   },
-  {
+  milgauss: {
     href: "#",
-    srcset:
+    roller_srcset:
       "https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0012_m116400gv-0002-milgauss.jpg?imwidth=550,%20https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0012_m116400gv-0002-milgauss.jpg?imwidth=1080%202x",
-    src: "/src/assets/media/img/img-watches-roller/watches-milgauss.webp",
-    alt: "Rolex milgauss watch",
-    title: "milgauss",
-    about: "Homage to science",
+    roller_src: "/src/assets/media/img/img-watches-roller/watches-milgauss.webp",
+    img_alt: "Rolex milgauss watch",
+    roller_title: "milgauss",
+    roller_about: "Homage to science",
   },
-  {
+  cellini: {
     href: "#",
-    srcset:
+    roller_srcset:
       "https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0014_m50535-0002-cellini-moonphase.jpg?imwidth=550,%20https://content.rolex.com/dam/new-watches-2020/homepage/roller/all-watches/watches_0014_m50535-0002-cellini-moonphase.jpg?imwidth=1080%202x",
-    src: "/src/assets/media/img/img-watches-roller/watches-cellini-moonphase.webp",
-    alt: "Rolex cellini watch",
-    title: "cellini",
-    about: "The classic watch",
+    roller_src: "/src/assets/media/img/img-watches-roller/watches-cellini-moonphase.webp",
+    img_alt: "Rolex cellini watch",
+    roller_title: "cellini",
+    roller_about: "The classic watch",
   },
-];
+};
