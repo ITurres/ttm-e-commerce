c1347cf0f304e5d75b53643879ca92500b25992d
ITurres <emanueliturres.co@gmail.com>
2022-11-11 20:57:10 -0300

    -test- roller with no /glider/

--- src/utils/home-page-utils.js
+++ src/utils/home-page-utils.js
@@ -1,5 +1,6 @@
 import "./navbar.js";
 import "./sidepanel.js";
-import "./watches-roller.js";
-import "./sidepanel-roller.js";
+// import "./items-roller.js";
+// import "./watches-roller.js";
+// import "./sidepanel-roller.js";
 import "./get-item-id.js";
---
+++ src/utils/items-roller.js
@@ -0,0 +1,38 @@
+// const $itemRollerContainer = document.querySelector("[data-roller]");
+
+// $itemRollerContainer.scrollTop = 100;
+// $itemRollerContainer.scrollLeft = 150;
+
+// let positions = { top: 0, left: 0, x: 0, y: 0 };
+
+// const mouseDownHandler = (event) => {
+//   //? Change the cursor and prevent user from selecting the text
+//   $itemRollerContainer.style.cursor = "grabbing";
+//   $itemRollerContainer.style.userSelect = "none";
+//   positions = {
+//     //?The current scroll
+//     left: $itemRollerContainer.scrollLeft,
+//     top: $itemRollerContainer.scrollTop,
+//     //?Get the current mouse position
+//     x: event.clientX,
+//     y: event.clientY,
+//   };
+//   document.addEventListener("mousemove", mouseMoveHandler);
+//   document.addEventListener("mouseup", mouseUpHandler);
+// };
+
+// const mouseMoveHandler = (event) => {
+//   //? How far the mouse has been moved
+//   const dx = event.clientX - positions.x;
+//   const dy = event.clientY - positions.y;
+//   //? Scroll the element
+//   $itemRollerContainer.scrollTop = positions.top - dy;
+//   $itemRollerContainer.scrollLeft = positions.left - dx;
+// };
+
+// const mouseUpHandler = () => {
+//   document.addEventListener("mousemove", mouseMoveHandler);
+//   document.addEventListener("mouseup", mouseUpHandler);
+//   $itemRollerContainer.style.cursor = "grab";
+//   $itemRollerContainer.style.removeProperty("user-select");
+// };
