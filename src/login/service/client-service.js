7b80f97b44326c5afc0df1ece5f19c0bbc7e7488
ITurres <emanueliturres.co@gmail.com>
2022-11-16 20:12:37 -0300

    Modify to new code on file

--- src/login/service/client-service.js
+++ src/login/service/client-service.js
@@ -3,49 +3,6 @@ const userList = async () => {
   return await response.json();
 };
 
-// userList().then((data) => {
-//   console.log(data);
-//   data.forEach((user) => {
-//     console.log(user);
-//   });
-// });
-
-const loginForm = document.querySelector(".modal-content");
-
-// const submit = document.querySelector("[data-submit]");
-
-loginForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  dataToValidate();
-});
-
-const inputUsername = document.querySelector("[data-username]");
-const inputUserPassword = document.querySelector("[data-user-password]");
-
-const dataToValidate = () => {
-  const inputUn = inputUsername.value;
-  const inputPw = inputUserPassword.value;
-  userList().then((users) => {
-    users.forEach((user) => {
-      if (inputUn === user.username && inputPw === user.password) {
-        validateLogin(inputUn, inputPw, user.username, user.password);
-      } else {
-        console.log("acces denied");
-
-        inputUsername.classList.toggle("input--invalid");
-        inputUserPassword.classList.toggle("input--invalid");
-      }
-    });
-  });
-};
-
-const validateLogin = (inputUn, inputPw, userUn, userPw) => {
-  if (inputUn === userUn && inputPw === userPw) {
-    console.log("acces granted");
-    //!HERE GOES HREF FOR ADMINISTRATOR PAGE (TO BE SET)//
-    inputUsername.value = "";
-    inputUserPassword.value = "";
-    inputUsername.classList.toggle("input--invalid");
-    inputUserPassword.classList.toggle("input--invalid");
-  }
+export const clientServices = {
+  userList,
 };
