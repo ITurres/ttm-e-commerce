3a259e1b8b48fac0d42b330decb41c857780591d
ITurres <emanueliturres.co@gmail.com>
2022-11-21 15:10:38 -0300

    Modify imports

--- src/components/admin-page-markup-componenets.js
+++ src/components/admin-page-markup-componenets.js
@@ -1,3 +1,2 @@
 import "./loaders/load-admin-items.js";
-import "./loaders/load-admin-edit-panel.js";
-import "../utils/toggle-admin-edit-panel.js";
+import "../admin-controller/admin-edit-item.js";
