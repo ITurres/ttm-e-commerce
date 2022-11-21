ef04f03d0bd3eff4ee6d4762a0dd590641f82365
ITurres <emanueliturres.co@gmail.com>
2022-11-21 15:16:07 -0300

    Add inject && update data functions

--- src/admin-controller/admin-edit-item.js
+++ src/admin-controller/admin-edit-item.js
@@ -2,35 +2,94 @@ import { adminServices } from "../login/service/admin-service.js";
 import { toggleEditPanel } from "../utils/toggle-admin-edit-panel.js";
 import { adminEditInputTemplates } from "../components/markup-templates/admin-edit-panel-template.js";
 
-const $inputsHolder = document.querySelector("[data-inputs-holder]");
-const $itemsInput = document.querySelectorAll("[data-item-data]");
-
-export const editItem = () => {
+export const itemToUpdate = () => {
   const $editItemBtn = document.querySelectorAll("[data-edit-item-btn]");
   $editItemBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
-      injectItemsData(btn.id);
+      getItemData(btn.id); //?passes btn's id clicked for comparison against element's id - see line 22-//
       toggleEditPanel();
+      updateItems();
     });
   });
 };
 
-const injectItemsData = (btnId) => {
+const getItemData = (btnId) => {
   adminServices.watchItems().then((items) => {
-    let inputValue = [];
     items.forEach((item) => {
       if (item.id === btnId) {
-        Object.keys(items[0]).map((itemKey) => {
+        const inputValue = [];
+        //?items[0] cause i just need the keys from only one of the elements (since each-one has the same)//
+        Object.keys(item).map((itemKey) => {
           Object.values(item).forEach((itemVal) => {
-            inputValue.push(itemVal);
+            inputValue.push(itemVal); //?stores each individual itemVal temporarily//
           });
-          $inputsHolder.innerHTML += adminEditInputTemplates(
-            item.id,
-            itemKey,
-            inputValue.shift()
-          );
+          injectItemsData(item.id, itemKey, inputValue.shift()); //?the itemVal temporarily stored gets use here -when shift-//
         });
       }
     });
+  });
+};
+
+const injectItemsData = (itemId, itemKey, inputValue) => {
+  const $inputsHolder = document.querySelector("[data-inputs-holder]");
+  $inputsHolder.innerHTML += adminEditInputTemplates(
+    itemId,
+    itemKey,
+    inputValue
+  );
+};
+
+//? all this constants are -inputs-, we need to find a way to loop throught them and retrieve each individual value,
+//? to be send to the -updateWatchData- function on admin-service.js. NOTE*: forEach works, but -updateWatchData- only receives
+//? first element!...maybe a loop within >??-updateWatchData-??< but where?...(TO BE FIXED) //
+export const updateItems = () => {
+  const $form = document.querySelector("[data-edit-item-form]");
+  $form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const id = document.querySelector(".id").value;
+    const page_href = document.querySelector(".page_href").value;
+    const roller_srcset = document.querySelector(".roller_srcset").value;
+    const roller_src = document.querySelector(".roller_src").value;
+    const img_alt = document.querySelector(".img_alt").value;
+    const roller_title = document.querySelector(".roller_title").value;
+    const roller_about = document.querySelector(".roller_about").value;
+    const video_src = document.querySelector(".video_src").value;
+    const video_srcset = document.querySelector(".video_srcset").value;
+    const header_title = document.querySelector(".header_title").value;
+    const header_subtitle = document.querySelector(".header_subtitle").value;
+    const about_title = document.querySelector(".about_title").value;
+    const about_text = document.querySelector(".about_text").value;
+    const watch_price = document.querySelector(".watch_price").value;
+    const img_srcset = document.querySelector(".img_srcset").value;
+    const img_src = document.querySelector(".img_src").value;
+    const style_class = document.querySelector(".style_class").value;
+    const sidepanel_class = document.querySelector(".sidepanel_class").value;
+    const doc_title = document.querySelector(".doc_title").value;
+    adminServices
+      .updateWatchData(
+        id,
+        page_href,
+        roller_srcset,
+        roller_src,
+        img_alt,
+        roller_title,
+        roller_about,
+        video_src,
+        video_srcset,
+        header_title,
+        header_subtitle,
+        about_title,
+        about_text,
+        watch_price,
+        img_srcset,
+        img_src,
+        style_class,
+        sidepanel_class,
+        doc_title
+      )
+      .then(() => {
+        console.log("Updated");
+      })
+      .catch((error) => error);
   });
 };
--- src/components/loaders/load-admin-items.js
+++ src/components/loaders/load-admin-items.js
@@ -1,17 +1,23 @@
 import { adminServices } from "../../login/service/admin-service.js";
 import { adminItemsTemplate } from "../markup-templates/admin-items-template.js";
+import { itemToUpdate } from "../../admin-controller/admin-edit-item.js";
 
 const $itemsSection = document.querySelector("[data-grid-items-section]");
 
-adminServices.watchItems().then((items) =>
-  items.forEach((item) => {
-    $itemsSection.innerHTML += adminItemsTemplate(
-      item.id,
-      item.roller_srcset,
-      item.roller_src,
-      item.img_alt,
-      item.roller_title,
-      item.roller_about
-    );
-  })
-);
+adminServices
+  .watchItems()
+  .then((items) =>
+    items.forEach((item) => {
+      $itemsSection.innerHTML += adminItemsTemplate(
+        item.id,
+        item.roller_srcset,
+        item.roller_src,
+        item.img_alt,
+        item.roller_title,
+        item.roller_about
+      );
+    })
+  )
+  .then(() => {
+    itemToUpdate();
+  });
