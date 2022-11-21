f30e371319a455ee7dd56a6158bb169363231847
ITurres <emanueliturres.co@gmail.com>
2022-11-21 15:13:27 -0300

    Add new function to adminServices

--- src/login/service/admin-service.js
+++ src/login/service/admin-service.js
@@ -3,6 +3,59 @@ const watchItems = async () => {
   return await response.json();
 };
 
+const updateWatchData = (
+  id,
+  page_href,
+  roller_srcset,
+  roller_src,
+  img_alt,
+  roller_title,
+  roller_about,
+  video_src,
+  video_srcset,
+  header_title,
+  header_subtitle,
+  about_title,
+  about_text,
+  watch_price,
+  img_srcset,
+  img_src,
+  style_class,
+  sidepanel_class,
+  doc_title
+) => {
+  fetch(`http://localhost:3000/watches_data/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+      page_href,
+      roller_srcset,
+      roller_src,
+      img_alt,
+      roller_title,
+      roller_about,
+      video_src,
+      video_srcset,
+      header_title,
+      header_subtitle,
+      about_title,
+      about_text,
+      watch_price,
+      img_srcset,
+      img_src,
+      style_class,
+      sidepanel_class,
+      doc_title,
+    }),
+  })
+    .then((response) => response)
+    .catch((error) => error);
+};
+
 export const adminServices = {
   watchItems,
+  updateWatchData,
 };
