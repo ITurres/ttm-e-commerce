59b84a7d5734bd70a5809be3253f9203357ea63d
ITurres <emanueliturres.co@gmail.com>
2022-11-22 13:53:33 -0300

    Add new fetch function to services

--- src/login/service/admin-service.js
+++ src/login/service/admin-service.js
@@ -3,6 +3,57 @@ const watchItems = async () => {
   return await response.json();
 };
 
+const addNewItem = (
+  page_href,
+  roller_srcset,
+  roller_src,
+  img_alt,
+  roller_title,
+  roller_about,
+  video_src,
+  video_srcset,
+  header_title,
+  header_subtitle,
+  about_title,
+  about_text,
+  watch_price,
+  img_srcset,
+  img_src,
+  style_class,
+  sidepanel_class,
+  doc_title
+) => {
+  return fetch("http://localhost:3000/watches_data", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: uuid.v4(),
+      page_href,
+      roller_srcset,
+      roller_src,
+      img_alt,
+      roller_title,
+      roller_about,
+      video_src,
+      video_srcset,
+      header_title,
+      header_subtitle,
+      about_title,
+      about_text,
+      watch_price,
+      img_srcset,
+      img_src,
+      style_class,
+      sidepanel_class,
+      doc_title,
+    }),
+  })
+    .then((response) => response)
+    .catch((error) => error);
+};
+
 const updateWatchData = (
   id,
   page_href,
@@ -24,7 +75,7 @@ const updateWatchData = (
   sidepanel_class,
   doc_title
 ) => {
-  fetch(`http://localhost:3000/watches_data/${id}`, {
+  return fetch(`http://localhost:3000/watches_data/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -56,13 +107,14 @@ const updateWatchData = (
 };
 
 const deleteItem = (itemId) => {
-  fetch(`http://localhost:3000/watches_data/${itemId}`, {
+  return fetch(`http://localhost:3000/watches_data/${itemId}`, {
     method: "DELETE",
   });
 };
 
 export const adminServices = {
   watchItems,
+  addNewItem,
   updateWatchData,
   deleteItem,
 };
