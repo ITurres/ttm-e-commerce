af6a4c13cfd51eba309fb1f380782bf82518aa3c
ITurres <emanueliturres.co@gmail.com>
2022-11-23 18:46:29 -0300

    Import new splide-roller to main -utils- script

--- src/utils/watches-pages-utils.js
+++ src/utils/watches-pages-utils.js
@@ -1,5 +1,6 @@
 import "./navbar.js";
 import "./sidepanel.js";
+import "./splide-roller/watch-pages-splide-roller.js";
 import "./get-item-id.js";
 import "./toggle-login.js";
 //!item below - DISABLED DUE TO CSS ROLLER ONLY - CHECK section-roller.css/section-roller-breakpoints.css//
