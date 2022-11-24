c74e21f6175f6287a7046e7707d870b90a2eba34
ITurres <emanueliturres.co@gmail.com>
2022-11-24 19:31:36 -0300

    Import links template loader on both html files

--- src/components/home-page-markup-components.js
+++ src/components/home-page-markup-components.js
@@ -2,3 +2,4 @@ import "./loaders/load-section-roller-data.js";
 import "./loaders/load-sidepanel-roller.js";
 import "./loaders/load-login-modal.js";
 import "./loaders/load-footer-watch-links.js";
+import "./loaders/load-social-media-links.js";
--- src/components/watch-pages-markup-components.js
+++ src/components/watch-pages-markup-components.js
@@ -2,3 +2,4 @@ import "./loaders/load-watch-page.js";
 import "./loaders/load-sidepanel-roller.js";
 import "./loaders/load-login-modal.js";
 import "./loaders/load-footer-watch-links.js";
+import "./loaders/load-social-media-links.js";
