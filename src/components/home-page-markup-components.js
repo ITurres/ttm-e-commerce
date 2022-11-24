f17b0eb9b021d7e74d03604e25529e01f4b859ec
ITurres <emanueliturres.co@gmail.com>
2022-11-24 18:54:15 -0300

    Import loaders to both scripts

--- src/components/home-page-markup-components.js
+++ src/components/home-page-markup-components.js
@@ -1,3 +1,4 @@
 import "./loaders/load-section-roller-data.js";
 import "./loaders/load-sidepanel-roller.js";
 import "./loaders/load-login-modal.js";
+import "./loaders/load-footer-watch-links.js";
--- src/components/watch-pages-markup-components.js
+++ src/components/watch-pages-markup-components.js
@@ -1,3 +1,4 @@
 import "./loaders/load-watch-page.js";
 import "./loaders/load-sidepanel-roller.js";
 import "./loaders/load-login-modal.js";
+import "./loaders/load-footer-watch-links.js";
