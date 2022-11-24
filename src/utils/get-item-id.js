a224086f5c60362d5ac9030933eebffa7480e46b
ITurres <emanueliturres.co@gmail.com>
2022-11-24 14:00:48 -0300

    Import adminServices to fetch data to create element

--- src/utils/get-item-id.js
+++ src/utils/get-item-id.js
@@ -1,7 +1,22 @@
-const $watchItems = document.querySelectorAll("[data-watch-item]");
+import { adminServices } from "../login/service/admin-service.js";
 
-$watchItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    localStorage.setItem("watchId", item.id);
+//!accesing to the data been fetch and callinf the function in either case (fetch succesfully or not)
+//! is important, since in both cases, the -constant- $watchItems won't exist when page loads..//
+adminServices
+  .watchItems()
+  .then((data) => {
+    getItemId(); //?if data from watches-data.json is fetched succefully => setItem//
+  })
+  .catch((error) => {
+    getItemId(); //?if NOT => also setItem//
   });
-});
+
+const getItemId = () => {
+  const $watchItems = document.querySelectorAll("[data-watch-item]");
+  $watchItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      console.log(item.id);
+      localStorage.setItem("watchId", item.id);
+    });
+  });
+};
