3021a3e7ceb34e6380bb2f724942eb9362a46fe2
ITurres <emanueliturres.co@gmail.com>
2022-11-24 19:30:02 -0300

    Create new object to hold links data

---
+++ src/components/social-media-data.js
@@ -0,0 +1,27 @@
+export const socialMediaData = {
+  facebook: {
+    i_Class: "fa-facebook-f",
+    page_href: "https://www.facebook.com/rolex",
+    title: "Facebook",
+  },
+  instagram: {
+    i_Class: "fa-instagram",
+    page_href: "https://www.instagram.com/rolex/",
+    title: "Instagram",
+  },
+  twitter: {
+    i_Class: "fa-twitter",
+    page_href: "https://twitter.com/rolex",
+    title: "Twitter",
+  },
+  youtube: {
+    i_Class: "fa-youtube",
+    page_href: "https://www.youtube.com/rolex",
+    title: "Youtube",
+  },
+  linkedin: {
+    i_Class: "fa-linkedin-in",
+    page_href: "https://www.linkedin.com/company/rolex",
+    title: "Linkedin",
+  },
+};
