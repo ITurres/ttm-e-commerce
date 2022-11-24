b2c89ede6064563a5b38bfc285028853def46ba4
ITurres <emanueliturres.co@gmail.com>
2022-11-24 19:31:05 -0300

    Add template loader for social-media-link

---
+++ src/components/loaders/load-social-media-links.js
@@ -0,0 +1,14 @@
+import { socialMediaData } from "../social-media-data.js";
+import { socialMediaLinksTemplate } from "../markup-templates/social-media-links-template.js";
+
+const $socialMediaLinksHolder = document.querySelector(
+  "[data-social-media-links-holder]"
+);
+
+Object.values(socialMediaData).forEach((link) => {
+  $socialMediaLinksHolder.innerHTML += socialMediaLinksTemplate(
+    link.i_Class,
+    link.page_href,
+    link.title
+  );
+});
