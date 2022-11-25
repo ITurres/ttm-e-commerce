0c7c21bd3d94415d6d573ccb7223dcf7a7ba5ff7
ITurres <emanueliturres.co@gmail.com>
2022-11-25 15:10:56 -0300

    Add search-engine script to home-utils file

--- src/utils/home-page-utils.js
+++ src/utils/home-page-utils.js
@@ -3,6 +3,7 @@ import "./sidepanel.js";
 import "./get-item-id.js";
 import "./toggle-login.js";
 import "./toggle-search-panel.js";
+import "./search-bar.js";
 //!items below / DISABLED DUE TO CSS ROLLER ONLY - CHECK section-roller.css/section-roller-breakpoints.css//
 // import "./items-roller.js";
 // import "./watches-roller.js";
