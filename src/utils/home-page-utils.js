71ffb195b0529070fa88a25a558af4c4d0f2a77b
ITurres <emanueliturres.co@gmail.com>
2022-11-25 11:26:22 -0300

    Import toggle script to main

--- src/utils/home-page-utils.js
+++ src/utils/home-page-utils.js
@@ -2,6 +2,7 @@ import "./navbar.js";
 import "./sidepanel.js";
 import "./get-item-id.js";
 import "./toggle-login.js";
+import "./toggle-search-panel.js";
 //!items below / DISABLED DUE TO CSS ROLLER ONLY - CHECK section-roller.css/section-roller-breakpoints.css//
 // import "./items-roller.js";
 // import "./watches-roller.js";
