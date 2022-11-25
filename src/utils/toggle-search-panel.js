78e9d2f16d1aff7d2ecc492e55d4f86a67ea886b
ITurres <emanueliturres.co@gmail.com>
2022-11-25 11:26:06 -0300

    Add toggle search-panel script

---
+++ src/utils/toggle-search-panel.js
@@ -0,0 +1,13 @@
+const $searchPanel = document.querySelector("[data-search-panel]");
+const $openSearchPanelBtn = document.querySelector("[data-open-search-panel]");
+const $closeSearchPanelBtn = document.querySelector(
+  "[data-close-search-panel]"
+);
+
+$openSearchPanelBtn.addEventListener("click", () => toggleSearchPanel());
+$closeSearchPanelBtn.addEventListener("click", () => toggleSearchPanel());
+
+const toggleSearchPanel = () => {
+  $searchPanel.classList.toggle("search-panel--show");
+  $searchPanel.style.transition = "800ms";
+};
