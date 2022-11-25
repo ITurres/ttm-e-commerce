649313fbf0993be6089874a3829a9e6f4bec8104
ITurres <emanueliturres.co@gmail.com>
2022-11-25 18:49:16 -0300

    Change -imports- order of precedence

--- src/utils/home-page-utils.js
+++ src/utils/home-page-utils.js
@@ -1,9 +1,9 @@
 import "./navbar.js";
 import "./sidepanel.js";
-import "./get-item-id.js";
-import "./toggle-login.js";
 import "./toggle-search-panel.js";
 import "./search-bar-engine.js";
+import "./get-item-id.js";
+import "./toggle-login.js";
 //!items below / DISABLED DUE TO CSS ROLLER ONLY - CHECK section-roller.css/section-roller-breakpoints.css//
 // import "./items-roller.js";
 // import "./watches-roller.js";
