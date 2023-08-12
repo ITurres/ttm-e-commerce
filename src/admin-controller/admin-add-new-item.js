a901404f2aa5e0aff27cb0492aa39c234656052b
ITurres <emanueliturres.co@gmail.com>
2023-08-11 21:00:56 -0300

    refactor/feat: on all -admin-controller- scripts - remove all selector constants and invoke 'createNewWatchDataObj' and more

--- src/admin-controller/admin-add-new-item.js
+++ src/admin-controller/admin-add-new-item.js
@@ -1,13 +1,14 @@
-import { adminServices } from "../login/service/admin-service.js";
-import { injectItemsData } from "./admin-edit-item.js";
-import { togglePanelAddNew } from "../utils/toggle-admin-panels.js";
+import { adminServices } from '../login/service/admin-service.js';
+import { injectItemsData } from './admin-edit-item.js';
+import { togglePanelAddNew } from '../utils/toggle-admin-panels.js';
+import { createNewWatchDataObj } from '../utils/create-new-watch-data-obj.js';
 
-const $addNewItemBtn = document.querySelector("[data-add-new-item-btn]");
-const $addNewInputsHolder = document.querySelector(
-  "[data-add-new-inputs-holder]"
+const addNewItemBtn = document.querySelector('[data-add-new-item-btn]');
+const addNewInputsHolder = document.querySelector(
+  '[data-add-new-inputs-holder]'
 );
 
-$addNewItemBtn.addEventListener("click", () => {
+addNewItemBtn.addEventListener('click', () => {
   injectItemsKey();
   togglePanelAddNew();
 });
@@ -16,57 +17,24 @@ const injectItemsKey = () => {
   adminServices.watchItems().then((item) => {
     //?item[0] cause i just need the keys from only one of the elements (since each-one has the same)//
     Object.keys(item[0]).map((itemKey) => {
-      injectItemsData($addNewInputsHolder, "", itemKey, ""); //?will inject the inputs with labels and placeholders text//
+      injectItemsData(addNewInputsHolder, '', itemKey, ''); //?will inject the inputs with labels and placeholders text//
     });
   });
 };
 
-const $form = document.querySelector("[data-add-new-item-form]");
-$form.addEventListener("submit", (event) => {
+const $form = document.querySelector('[data-add-new-item-form]');
+$form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const page_href = document.querySelector(".page_href").value;
-  const roller_srcset = document.querySelector(".roller_srcset").value;
-  const roller_src = document.querySelector(".roller_src").value;
-  const img_alt = document.querySelector(".img_alt").value;
-  const roller_title = document.querySelector(".roller_title").value;
-  const roller_about = document.querySelector(".roller_about").value;
-  const video_src = document.querySelector(".video_src").value;
-  const video_srcset = document.querySelector(".video_srcset").value;
-  const header_title = document.querySelector(".header_title").value;
-  const header_subtitle = document.querySelector(".header_subtitle").value;
-  const about_title = document.querySelector(".about_title").value;
-  const about_text = document.querySelector(".about_text").value;
-  const watch_price = document.querySelector(".watch_price").value;
-  const img_srcset = document.querySelector(".img_srcset").value;
-  const img_src = document.querySelector(".img_src").value;
-  const style_class = document.querySelector(".style_class").value;
-  const sidepanel_class = document.querySelector(".sidepanel_class").value;
-  const doc_title = document.querySelector(".doc_title").value;
+  const itemInputs = document.querySelectorAll('[data-admin-item-inputs]');
+
+  const newWatchData = createNewWatchDataObj(itemInputs);
+
   adminServices
-    .addNewItem(
-      page_href,
-      roller_srcset,
-      roller_src,
-      img_alt,
-      roller_title,
-      roller_about,
-      video_src,
-      video_srcset,
-      header_title,
-      header_subtitle,
-      about_title,
-      about_text,
-      watch_price,
-      img_srcset,
-      img_src,
-      style_class,
-      sidepanel_class,
-      doc_title
-    )
+    .addNewItem(newWatchData)
     .then((respuesta) => {
-      console.log(respuesta);
+      console.info(respuesta);
     })
     .catch((error) =>
-      console.log(`There was an error trying to fetch data ==> ${error}`)
+      console.error(`There was an error trying to fetch data ==> ${error}`)
     );
 });
--- src/admin-controller/admin-edit-item.js
+++ src/admin-controller/admin-edit-item.js
@@ -1,13 +1,14 @@
-import { adminServices } from "../login/service/admin-service.js";
-import { togglePanelEdit } from "../utils/toggle-admin-panels.js";
-import { adminEditInputTemplates } from "../components/markup-templates/admin-edit-panel-template.js";
+import { adminServices } from '../login/service/admin-service.js';
+import { togglePanelEdit } from '../utils/toggle-admin-panels.js';
+import { adminEditInputTemplates } from '../components/markup-templates/admin-edit-panel-template.js';
+import { createNewWatchDataObj } from '../utils/create-new-watch-data-obj.js';
 
-const $editInputsHolder = document.querySelector("[data-edit-inputs-holder]");
+const $editInputsHolder = document.querySelector('[data-edit-inputs-holder]');
 
 export const itemToUpdate = () => {
-  const $editItemBtn = document.querySelectorAll("[data-edit-item-btn]");
+  const $editItemBtn = document.querySelectorAll('[data-edit-item-btn]');
   $editItemBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener('click', () => {
       getItemData(btn.id); //?passes btn's id clicked for comparison against element's id - see line 22-//
       togglePanelEdit();
       updateItems();
@@ -37,7 +38,7 @@ export const getItemData = (btnId) => {
       });
     })
     .catch((error) =>
-      console.log(`There was an error trying to fetch data ==> ${error}`)
+      console.error(`There was an error trying to fetch data ==> ${error}`)
     );
 };
 
@@ -45,59 +46,22 @@ export const injectItemsData = (holder, itemId, itemKey, inputValue) => {
   holder.innerHTML += adminEditInputTemplates(itemId, itemKey, inputValue);
 };
 
-//? all this constants are -inputs-, I need to find a way to loop throught them and retrieve each individual value,
-//? to be send to the -updateWatchData- function on admin-service.js. NOTE*: forEach works, but -updateWatchData- only receives
-//? first element!...maybe a loop within >??-updateWatchData-??< but where?...(TO BE REFACTORED) //
 export const updateItems = () => {
-  const $form = document.querySelector("[data-edit-item-form]");
-  $form.addEventListener("submit", (event) => {
+  const $form = document.querySelector('[data-edit-item-form]');
+  $form.addEventListener('submit', (event) => {
     event.preventDefault();
-    const id = document.querySelector(".id").value;
-    const page_href = document.querySelector(".page_href").value;
-    const roller_srcset = document.querySelector(".roller_srcset").value;
-    const roller_src = document.querySelector(".roller_src").value;
-    const img_alt = document.querySelector(".img_alt").value;
-    const roller_title = document.querySelector(".roller_title").value;
-    const roller_about = document.querySelector(".roller_about").value;
-    const video_src = document.querySelector(".video_src").value;
-    const video_srcset = document.querySelector(".video_srcset").value;
-    const header_title = document.querySelector(".header_title").value;
-    const header_subtitle = document.querySelector(".header_subtitle").value;
-    const about_title = document.querySelector(".about_title").value;
-    const about_text = document.querySelector(".about_text").value;
-    const watch_price = document.querySelector(".watch_price").value;
-    const img_srcset = document.querySelector(".img_srcset").value;
-    const img_src = document.querySelector(".img_src").value;
-    const style_class = document.querySelector(".style_class").value;
-    const sidepanel_class = document.querySelector(".sidepanel_class").value;
-    const doc_title = document.querySelector(".doc_title").value;
+
+    const itemInputs = document.querySelectorAll('[data-admin-item-inputs]');
+
+    const newWatchData = createNewWatchDataObj(itemInputs);
+
     adminServices
-      .updateWatchData(
-        id,
-        page_href,
-        roller_srcset,
-        roller_src,
-        img_alt,
-        roller_title,
-        roller_about,
-        video_src,
-        video_srcset,
-        header_title,
-        header_subtitle,
-        about_title,
-        about_text,
-        watch_price,
-        img_srcset,
-        img_src,
-        style_class,
-        sidepanel_class,
-        doc_title
-      )
+      .updateWatchData(newWatchData)
       .then((response) => {
-        console.log("Updated", response);
+        console.info('Updated', response);
       })
       .catch((error) =>
-        console.log(`There was an error trying to fetch data ==> ${error}`)
+        console.error(`There was an error trying to fetch data ==> ${error}`)
       );
   });
 };
