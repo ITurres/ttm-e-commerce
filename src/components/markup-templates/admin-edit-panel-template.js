0dcdb9182f9a68a6d67731f4dc1c85c68cf7be87
ITurres <emanueliturres.co@gmail.com>
2023-08-11 21:02:31 -0300

    refactor: on 'admin-edit-panel-template.js' Rename data-attribute

--- src/components/markup-templates/admin-edit-panel-template.js
+++ src/components/markup-templates/admin-edit-panel-template.js
@@ -1,5 +1,5 @@
 export const adminEditInputTemplates = (itemId, dataKey, keyValue) => {
   return `<label for="${dataKey}"><b class="label-title">${dataKey}</b></label>
     <input id="${itemId}" class="data-input ${dataKey}" type="text" placeholder="Set here item's -${dataKey}-"
-     name="${dataKey}" value="${keyValue}" required data-item-data/>`;
+     name="${dataKey}" value="${keyValue}" required data-admin-item-inputs/>`;
 };
