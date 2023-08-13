5a073b4cd5d700ed2207ed2b5293393aac4be267
ITurres <emanueliturres.co@gmail.com>
2023-08-12 23:59:26 -0300

    refactor: on 'search-bar-engine.js' functionality and enhance code structure
    - Introduced a dedicated function for creating item elements
    - Improved variable naming for better readability
    - Streamlined search input handling and item rendering
    - Utilized the itemsFoundData function for consistent data presentation
    
    This commit improves the modularity and clarity of the search feature code, making it easier to maintain and understand.

--- src/utils/search-bar-engine.js
+++ src/utils/search-bar-engine.js
@@ -1,25 +1,43 @@
-import { adminServices } from "../login/service/admin-service.js";
-import { watches_data } from "../components/watches-data.js";
+import { adminServices } from '../login/service/admin-service.js';
+import { watches_data } from '../components/watches-data.js';
 
-const $searchPanel = document.querySelector("[data-search-panel]");
-const $itemResultTemplate = document.querySelector("[data-item-template]");
-const $resultsContainer = document.querySelector("[data-results-container]");
-const $searchInput = document.querySelector("[data-search-input]");
+const searchPanel = document.querySelector('[data-search-panel]');
+const itemResultTemplate = document.querySelector('[data-item-template]');
+const resultsContainer = document.querySelector('[data-results-container]');
+const searchInput = document.querySelector('[data-search-input]');
 let itemsFound = [];
 
-$searchInput.addEventListener("input", (e) => {
+const itemsFoundData = (item) => {
+  const itemResultCard = itemResultTemplate.content.cloneNode(true).children[0];
+  const itemAnchor = itemResultCard.querySelector('[data-watch-item]');
+  const itemImg = itemResultCard.querySelector('[data-item-img]');
+  const itemTitle = itemResultCard.querySelector('[data-item-title]');
+  const itemAbout = itemResultCard.querySelector('[data-item-about]');
+  itemAnchor.id = item.id;
+  itemImg.src = item.roller_src;
+  itemTitle.textContent = item.roller_title;
+  itemAbout.textContent = item.roller_about;
+  resultsContainer.append(itemResultCard);
+  return {
+    name: item.roller_title,
+    name2: item.roller_about,
+    element: itemResultCard,
+  };
+};
+
+searchInput.addEventListener('input', (e) => {
   const inputValue = e.target.value.toLowerCase();
-  if (inputValue === "") {
-    $resultsContainer.style.display = "none";
-    $searchPanel.style.height = "fit-content";
+  if (inputValue === '') {
+    resultsContainer.style.display = 'none';
+    searchPanel.style.height = 'fit-content';
   } else {
-    $searchPanel.style.height = "100vh";
-    $resultsContainer.style.display = "grid";
+    searchPanel.style.height = '100vh';
+    resultsContainer.style.display = 'grid';
     itemsFound.forEach((item) => {
       const isItThere =
         item.name.toLowerCase().includes(inputValue) ||
         item.name2.toLowerCase().includes(inputValue);
-      item.element.classList.toggle("show-results", isItThere);
+      item.element.classList.toggle('show-results', isItThere);
     });
   }
 });
@@ -27,47 +45,15 @@ $searchInput.addEventListener("input", (e) => {
 adminServices
   .watchItems()
   .then((items) => {
-    itemsFound = items.map((item) => {
-      const $itemResultCard =
-        $itemResultTemplate.content.cloneNode(true).children[0];
-      const itemAnchor = $itemResultCard.querySelector("[data-watch-item]");
-      const itemImg = $itemResultCard.querySelector("[data-item-img]");
-      const itemTitle = $itemResultCard.querySelector("[data-item-title]");
-      const itemAbout = $itemResultCard.querySelector("[data-item-about]");
-      itemAnchor.id = item.id;
-      itemImg.src = item.roller_src;
-      itemTitle.textContent = item.roller_title;
-      itemAbout.textContent = item.roller_about;
-      $resultsContainer.append($itemResultCard);
-      return {
-        name: item.roller_title,
-        name2: item.roller_about,
-        element: $itemResultCard,
-      };
-    });
-    console.log("The Search results were render from => JSON-server-data");
+    itemsFound = items.map((item) => itemsFoundData(item));
+    console.info('The Search results were render from => JSON-server-data');
   })
   .catch((error) => {
-    itemsFound = Object.values(watches_data).map((item) => {
-      const $itemResultCard =
-        $itemResultTemplate.content.cloneNode(true).children[0];
-      const itemAnchor = $itemResultCard.querySelector("[data-watch-item]");
-      const itemImg = $itemResultCard.querySelector("[data-item-img]");
-      const itemTitle = $itemResultCard.querySelector("[data-item-title]");
-      const itemAbout = $itemResultCard.querySelector("[data-item-about]");
-      itemAnchor.id = item.id;
-      itemImg.src = item.roller_src;
-      itemTitle.textContent = item.roller_title;
-      itemAbout.textContent = item.roller_about;
-      $resultsContainer.append($itemResultCard);
-      return {
-        name: item.roller_title,
-        name2: item.roller_about,
-        element: $itemResultCard,
-      };
-    });
-    console.log(
-      "The search results were render from => JS-static-data | data from JSON-server got an error =>",
+    itemsFound = Object.values(watches_data).map((item) =>
+      itemsFoundData(item)
+    );
+    console.info(
+      'The search results were render from => JS-static-data | data from JSON-server got an error =>',
       error
     );
   });
