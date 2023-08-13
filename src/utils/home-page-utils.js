c488128690fe58076304f91dbfd6b615c8211d41
ITurres <emanueliturres.co@gmail.com>
2023-08-12 23:32:22 -0300

    chore: Remove all unused scripts and commented code

--- src/utils/home-page-utils.js
+++ src/utils/home-page-utils.js
@@ -1,10 +1,6 @@
-import "./navbar.js";
-import "./sidepanel.js";
-import "./toggle-search-panel.js";
-import "./search-bar-engine.js";
-import "./get-item-id.js";
-import "./toggle-login.js";
-//!items below / DISABLED DUE TO CSS ROLLER ONLY - CHECK section-roller.css/section-roller-breakpoints.css//
-// import "./items-roller.js";
-// import "./watches-roller.js";
-// import "./sidepanel-roller.js";
+import './navbar.js';
+import './sidepanel.js';
+import './toggle-search-panel.js';
+import './search-bar-engine.js';
+import './get-item-id.js';
+import './toggle-login.js';
--- src/utils/watches-pages-utils.js
+++ src/utils/watches-pages-utils.js
@@ -1,8 +1,6 @@
-import "./navbar.js";
-import "./sidepanel.js";
-import "./toggle-search-panel.js";
-import "./search-bar-engine.js";
-import "./get-item-id.js";
-import "./toggle-login.js";
-//!item below - DISABLED DUE TO CSS ROLLER ONLY - CHECK section-roller.css/section-roller-breakpoints.css//
-// import "./sidepanel-roller.js";
+import './navbar.js';
+import './sidepanel.js';
+import './toggle-search-panel.js';
+import './search-bar-engine.js';
+import './get-item-id.js';
+import './toggle-login.js';
